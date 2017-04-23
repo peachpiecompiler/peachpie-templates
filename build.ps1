aa1e37a67301ef4ce5014675e7e170bea0291b84
Petr Houška <houskape@gmail.com>
2017-04-23 20:50:32 +0200

    Added automatic testing

--- build.ps1
+++ build.ps1
@@ -2,9 +2,43 @@ Param(
   [string]$suffix = $env:PrereleaseTag
 )
 
+### build ###
+
 # nuget.exe pack
-$nugetargs = "pack", "./src/Peachpie.Templates.nuspec", "-OutputDirectory", "out"
+$nugetargs = "pack", "$PSScriptRoot/src/Peachpie.Templates.nuspec", "-OutputDirectory", "out"
 if ($suffix) { $nugetargs += "-Suffix", $suffix }
 ./tools/nuget.exe $nugetargs
 
-# TODO: test templates
+
+### tests ###
+
+# install templates
+dotnet new --debug:reinit
+dotnet new -i "$PSScriptRoot/out/Peachpie.Templates.*.nupkg"
+
+# prepare folder for templates testing
+mkdir "$PSScriptRoot/tmp"
+Set-Location "$PSScriptRoot/tmp"
+
+# get all available templates
+$templates = Get-ChildItem -dir "$PSScriptRoot/src/Content"
+
+$error = 0;
+
+# for each template create its project, restore it, and build it
+foreach($template in $templates){
+    Remove-Item "./*" -Recurse -Force
+
+    dotnet new $template.Name
+    if(!($?)) { $error += 1; continue }
+    dotnet restore 
+    if(!($?)) { $error += 1; continue }
+    dotnet build
+    if(!($?)) { $error += 1; continue }
+}
+
+# remove tmp folder
+Set-Location "$PSScriptRoot"
+Remove-Item "./tmp" -Force -Recurse
+
+exit $error
