293e772829a3e4bdb01706b636fe638542675d32
Jakub Míšek <jmisek@gmail.com>
2017-12-12 14:33:24 +0100

    version number variable

--- build/build.ps1
+++ build/build.ps1
@@ -1,8 +1,10 @@
 Param(
+  [string]$version,
   [string]$suffix
 )
 
 # nuget.exe pack
 $nugetargs = "pack", "$PSScriptRoot/../src/Peachpie.Templates.nuspec", "-OutputDirectory", "out"
+if ($version) { $nugetargs += "-Version", $version }
 if ($suffix) { $nugetargs += "-Suffix", $suffix }
 ./tools/nuget.exe $nugetargs
