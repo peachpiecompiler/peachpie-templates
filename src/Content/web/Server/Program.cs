1adc8d0fb781ded2c50a8ab37b1d9a4314482359
Jakub Míšek <jmisek@gmail.com>
2020-09-03 11:26:06 +0200

    website content gets published and served by server

--- src/Content/web/Server/Program.cs
+++ src/Content/web/Server/Program.cs
@@ -1,8 +1,10 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace MyWebsite.1.Server
@@ -41,11 +43,41 @@ namespace MyWebsite.1.Server
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
+            //// sample usage of URL rewrite:
+            //var options = new RewriteOptions()
+            //    .AddRewrite(@"^rule/(\w+)", "index.php?word=$1", skipRemainingRules: true);
+
+            //app.UseRewriter(options);
+
+            // enable session:
             app.UseSession();
 
-            app.UsePhp();
+            // enable .php files from compiled assembly:
+            var contentPath = ResolveContentPath();
+
+            app.UsePhp("/", rootPath: contentPath);
+            app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(contentPath) });
+            
+            //
             app.UseDefaultFiles();
             app.UseStaticFiles();
+        }
+
+        /// <summary>
+        /// Gets location of website project content.
+        /// In development, we use the original website project location.
+        /// Otherwise, content files are published to the current working directory.
+        /// </summary>
+        /// <returns></returns>
+        static string ResolveContentPath()
+        {
+            var devcontent = Path.GetFullPath("../Website");
+            if (Directory.Exists(devcontent))
+            {
+                return devcontent;
+            }
+
+            return Directory.GetCurrentDirectory();
         }
     }
 }
